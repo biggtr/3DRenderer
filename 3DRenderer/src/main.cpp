6e83c7f172b82f50b651540252462f92533ea280
biggtr <bigtorresg@gmail.com>
2024-03-12 17:02:32 +0200

    process input function has been added

--- 3DRenderer/src/main.cpp
+++ 3DRenderer/src/main.cpp
@@ -34,9 +34,58 @@ bool initialize_window()
 
 }
 
+void setup()
+{
+
+}
+
+void process_input()
+{
+	SDL_Event event;
+
+	switch (event.type)
+	{
+		case SDL_QUIT:
+			is_running = false;
+			break;
+
+		case SDL_KEYDOWN:
+			if (event.key.keysym.sym == SDLK_ESCAPE)
+				is_running = false;
+			break;
+
+	}
+
+}
+
+void update()
+{
+
+
+}
+
+void render()
+{
+
+
+}
+
+
+
+
 int main(int argc,char*args[])
 {
 	is_running = initialize_window();
+
+	setup();
+
+	while (is_running)
+	{
+		process_input();
+		update();
+		render();
+	}
+
 	std::cout << "Hello, world!";
 	return 0;
 }
