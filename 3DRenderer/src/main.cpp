9754b205a808c098ff8df788a614532b4d1b0460
biggtr <bigtorresg@gmail.com>
2024-03-15 20:39:27 +0200

    making vector struct and initializing cube with vectors

--- 3DRenderer/src/main.cpp
+++ 3DRenderer/src/main.cpp
@@ -1,8 +1,10 @@
 #include "display.h"
-
+#include "vector.h"
 
 bool is_running = false;
 
+int N_POINTS;
+vec3_t * cube_points = new vec3_t[N_POINTS];
 
 
 void setup()
@@ -10,6 +12,20 @@ void setup()
 	color_buffer = new uint32_t[WindowWidth * WindowHeight];
 
 	color_buffer_texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, WindowWidth, WindowHeight);
+
+	int points_count = 0;
+	//initialize cube vector with values with 9 points in each direction x,y,z 
+	for (float x = -1; x <= 1; x += 0.22) // each side is 2 size from -1 to 1  = 2; and we want to fill each with 9 points so 2/9 = 0.22
+	{
+		for (float y = -1; y <= 1; y += 0.22)
+		{
+			for (float z = -1; z <= 1; z += 0.22)
+			{
+				vec3_t new_point = { x,y,z };
+				cube_points[points_count++] = new_point;
+			}
+		}
+	}
 }
 
 void process_input()
@@ -44,6 +60,7 @@ void render()
 	SDL_RenderClear(renderer);
 	clear_color_buffer(0xFF000000);
 	draw_grid();
+	draw_pixel(100, 100, 0xFFFF0000);
 	draw_rectangle(100,600,400,200,0xFFFF0000);
 	render_color_buffer();
 	SDL_RenderPresent(renderer);
---
+++ 3DRenderer/src/vector.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+
+struct vec2_t
+{
+	float x;
+	float y;
+};
+
+struct vec3_t
+{
+	float x;
+	float y;
+	float z;
+};
