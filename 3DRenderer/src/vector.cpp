7a811374e2444f0c2e2418591e48244d32cc8fd8
biggtr <bigtorresg@gmail.com>
2024-03-17 14:35:39 +0200

    rotation transformation implemented

--- 3DRenderer/src/vector.cpp
+++ 3DRenderer/src/vector.cpp
@@ -1,1 +1,40 @@
+#include <math.h>
 #include "vector.h"
+
+
+
+vec3_t vec3_rotate_x(vec3_t point, float angle)
+{
+	vec3_t rotated_vector =
+	{
+		point.x,
+		point.y* cos(angle) - point.z * sin(angle),
+		point.y* sin(angle) + point.z * cos(angle),
+
+	};
+	return rotated_vector;
+}
+	
+
+vec3_t vec3_rotate_y(vec3_t point,float angle)
+{
+	vec3_t rotated_vector =
+	{
+		point.x * cos(angle) - point.z * sin(angle),
+		point.y,
+		point.x * sin(angle) + point.z * cos(angle),
+	};
+	return rotated_vector;
+}
+
+vec3_t vec3_rotate_z(vec3_t point, float angle)
+{			
+	vec3_t rotated_vector =
+	{
+		point.x * cos(angle) - point.y * sin(angle),
+		point.x * sin(angle) + point.y * cos(angle),
+		point.z
+	};
+	return rotated_vector;
+}
+
