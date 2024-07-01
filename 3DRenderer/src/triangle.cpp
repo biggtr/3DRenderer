ad67acfa39d238411df87f2da2400963436307b1
biggtr <bigtorresg@gmail.com>
2024-07-01 14:43:34 +0300

    added fillFlatTop function and fixed issue with floating point conversion with x and y

--- 3DRenderer/src/triangle.cpp
+++ 3DRenderer/src/triangle.cpp
@@ -5,7 +5,7 @@ void fillFlatBottom(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t col
 	float leftSideSlope = (float)(x1 - x0) / (y1 - y0);
 	float rightSideSlope = (float)(x2 - x0) / (y2 - y0);
 
-	int xStart{ x0 }, xEnd{ x0};
+	float xStart{ (float)x0 }, xEnd{ (float)x0};
 
 	for (int height = y0; height <= y2; height++)
 	{
@@ -16,7 +16,17 @@ void fillFlatBottom(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t col
 }
 void fillFlatTop(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color)
 {
+	float leftSideSlope = (float)(x2 - x0) / (y2 - y0);
+	float rightSideSlope = (float)(x2 - x1) / (y2 - y1);
 
+	float xStart{ (float)x2 }, xEnd{ (float)x2 };
+
+	for (int height = y2; height >= y0; height--)
+	{
+		drawLine(xStart, height, xEnd, height, color);
+		xStart -= leftSideSlope;
+		xEnd -= rightSideSlope;
+	}
 }
 
 void drawFilledTriangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color)
@@ -43,5 +53,5 @@ void drawFilledTriangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t
 	int yMidPoint = y1;
 
 	fillFlatBottom(x0,y0,x1,y1,xMidPoint,yMidPoint,color);
-	//fillFlatTop(x1,y1,xMidPoint,yMidPoint,x2,y2,color);
+	fillFlatTop(x1,y1,xMidPoint,yMidPoint,x2,y2,color);
 }
